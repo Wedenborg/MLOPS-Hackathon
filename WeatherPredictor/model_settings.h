a2f94767205d277399e884e5e852399d9e90d4a1
adria <adriamompo@gmail.com>
2025-08-22 14:01:07 +0200

    final weather predictor

--- WeatherPredictor/model_settings.h
+++ WeatherPredictor/model_settings.h
@@ -7,7 +7,7 @@
 // These values MUST match the input shape of the model you trained.
 
 // The number of past days of data your model uses for a single prediction.
-constexpr int kHistoryDays = 59; // e.g., if your model looks at the past 59 days
+constexpr int kHistoryDays = 14; // e.g., if your model looks at the past 59 days
 
 // The number of days being predicted (usually 1, for "today").
 constexpr int kFutureDays = 1;
